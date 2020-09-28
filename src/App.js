cf33709c5eb677eb0c326f05db4c6960e07f3cc4
Robert Bailey <robbailey@Robs-MacBook-Air.local>
2020-09-28 10:52:05 -0600

    Adds more functionality to math problems

--- src/App.js
+++ src/App.js
@@ -6,14 +6,42 @@ import Overlay from './Components/Overlay';
 function App() {
   const [score, setScore] = React.useState(0)
   const [mistakes, setMistakes] = React.useState(0)
+  const [currentProblem, setCurrentProblem] = React.useState(generateProblem())
+  const [userAnswer, setUserAnswer] = React.useState('')
+
+  function generateNumber(max) {
+    return Math.floor(Math.random() * (max + 1))
+  }
+  
+  function generateProblem() {
+    return {
+      numberOne: generateNumber(10),
+      numberTwo: generateNumber(10),
+      operator: ['+', '-', 'x'][generateNumber(2)]
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    let correctAnswer
+    if (currentProblem.operator == "+") correctAnswer = currentProblem.numberOne + currentProblem.numberTwo
+    if (currentProblem.operator == "-") correctAnswer = currentProblem.numberOne - currentProblem.numberTwo
+    if (currentProblem.operator == "x") correctAnswer = currentProblem.numberOne * currentProblem.numberTwo
+    if (correctAnswer == parseInt(userAnswer, 10)) {
+      setScore(prev => prev + 1)
+    } else {
+      setMistakes(prev => prev + 1)
+    }
+  }
 
   return (
     <>
       <div className="main-ui">
-        <p className="problem"></p>
+        <p className="problem"> {currentProblem.numberOne} {currentProblem.operator} {currentProblem.numberTwo} </p>
 
-        <form action="" className="answer-form">
-          <input type="text" className="answer-field" autoComplete="off" />
+        <form onSubmit={handleSubmit} action="" className="answer-form">
+          <input onChange={e => setUserAnswer(e.target.value)} type="text" className="answer-field" autoComplete="off" />
           <button>Submit</button>
         </form>
 
